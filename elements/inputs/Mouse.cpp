324f552c5437c07332415b9bcc7b213b0004af18
YusufMalu001 <yusuf.malu04@gmail.com>
2024-11-21 12:36:07 +0530

    mouse tracing

--- elements/inputs/Mouse.cpp
+++ elements/inputs/Mouse.cpp
@@ -2,16 +2,14 @@
 // Call Update In The Game Loop To Constantly Update The Current Mouse X,Y On The Screen, Draw A Custom Circle Or Overlay For The Mouse
 #include "Mouse.h"
 
-Mouse::Mouse() {
-    // Initialize the mouse position to the center of the screen
-    position = { 0, 0 };
-    // Hide the system mouse
-    HideCursor();
-}
+
+
 
 void Mouse::update() {
     // Update the mouse position based on its current location
     position = GetMousePosition();
+        HideCursor();
+
 }
 
 void Mouse::draw() {
