2b234546be149179adbc8c3c952c6eecfa16d450
yuyutsu01 <shivamofficial2377@gmail.com>
2024-12-05 12:17:18 +0530

    changed platform to wall/floor

--- levels/LevelOne.h
+++ levels/LevelOne.h
@@ -1,32 +1,7 @@
 #include "vector"
 #include <raylib.h>
-struct Platform {
-  float x;
-  float y;
-  float opacity;
-  float width;
-  float height;
-  bool enabledCollision;
-  bool isVisibleToPlayer;
-  bool drawPlatform;
-
-  // Constructor to initialize the struct
-  Platform(int x, int y, float opacity, float width, float height,
-           bool enabledCollision)
-      : x(x), y(y), opacity(opacity), width(width), height(height),
-        enabledCollision(enabledCollision), isVisibleToPlayer(true),
-        drawPlatform(true) {
-    if (drawPlatform) {
-      draw();
-    }
-  }
-
-  // Draw function to draw the platform
-  void draw() { DrawRectangle(x, y, width, height, GREEN); }
-
-  // Function to get the platform's rectangle
-};
-
+#include "elements/platforms/Wall.h"
+#include "elements/platforms/Floor.h"
 class LevelOne {
 private:
   int stageCount = 1;
@@ -34,15 +9,15 @@ private:
 
 public:
   void drawPlatforms() {
-    Platform p1(300, 400, 1.0, 300, 10, true);
+    Floor p1(300, 400, 1.0, 300, 10, true);
     platforms.push_back({300, 400, 300, 10});
-    Platform p2(200, 500, 1.0, 200, 10, true);
+    Floor p2(200, 500, 1.0, 200, 10, true);
     platforms.push_back({200, 500, 200, 10});
-    Platform p3(300, 600, 1.0, 400, 10, true);
+    Floor p3(300, 600, 1.0, 400, 10, true);
     platforms.push_back({300, 600, 400, 10});
-    Platform p4(300, 300, 1.0, 10, 600, true);
+    Wall p4(300, 300, 1.0, 10, 600, true);
     platforms.push_back({300, 300, 10, 600});
-    Platform p5(800, 300, 1.0, 10, 600, true);
+    Wall p5(800, 300, 1.0, 10, 600, true);
     platforms.push_back({800, 300, 10, 600});
 
 
